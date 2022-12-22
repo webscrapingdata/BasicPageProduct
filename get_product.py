fb2efc995f16ad961dde7e9b5771de22b95da4e4
Azizbek Qozoqov <73469043+azizbekQozoqov@users.noreply.github.com>
2022-12-22 10:10:05 +0000

    Done

--- get_product.py
+++ get_product.py
@@ -1,3 +1,5 @@
+from bs4 import BeautifulSoup
+
 def get_product(data:str) -> str:
     """Get the product name from the data string.
 
@@ -7,7 +9,7 @@ def get_product(data:str) -> str:
     Returns:
         str: The product name in CSV format.
     """
-    pass
+    return "Product Name,Product Price,Product Description\n"+("\n".join([f'{element.h3.text}, {element.p.text}, {element.find(class_="description").text}' for element in BeautifulSoup(data, features="html.parser").find_all(class_="product")]))
 
 def save_product(product:str) -> None:
     """Save the product name to the CSV file.
@@ -15,12 +17,14 @@ def save_product(product:str) -> None:
     Args:
         product (str): The product name in CSV format.
     """
-    pass
+    with open('product.csv', "w") as f: f.write(product)
+    return True
 
 # Open the file and read the HTML string
 with open('html/product1.html', 'r') as f:
     data = f.read()
     # Get the product name in CSV format
     product_csv=get_product(data) 
+    print(product_csv)
     # Save the product name to the CSV file
     save_product(product_csv) 
